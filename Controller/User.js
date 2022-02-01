8fd8ce7adeec42f380506e1ee2723077aa848063
Nofrisdan <nofrisdansitopu22@gmail.com>
2022-02-02 00:33:55 +0700

    Menambahkan modul getdata

--- Controller/User.js
+++ Controller/User.js
@@ -1,7 +1,7 @@
 'use strict';
 
-
 // connect to db
+const db = require("../database/connect");
 
 // layouts
 const layout = "layouts/User";
@@ -13,5 +13,27 @@ exports.index = function(req,res){
     res.render("User/dashboard",{
         layout : layout
     });
+}
+
+// get All data
+exports.getAll = function(req,res){
+    const sql = "SELECT * FROM parkir";
+
+    const result = db.query(sql,(err,results,fields)=> {
+        if(err) throw err;
+
+        res.json({value : results});
+
+        res.end();
+    })
+
     
+}
+
+
+// dwonload
+exports.dwonload = function(req,res){
+    // res.download("/dwonload/mobile.png","tes.png");
+    // res.download(__dirname + "")
+    res.download("./public/img/mobile.png");
 }
